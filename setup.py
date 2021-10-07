fc8703702a6064e33e397a65859f1e9dd190fd28
Anurag Kumar <mailanu98@gmail.com>
2021-10-08 03:28:50 +0530

    updated setup.py and .travis.yml (#10)
    * Update setup.py
    
    updated classifiers
    
    * Update .travis.yml
    
    updated with python versions

--- setup.py
+++ setup.py
@@ -99,6 +99,9 @@ setup(
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
         'Operating System :: POSIX :: Linux'
     ],
